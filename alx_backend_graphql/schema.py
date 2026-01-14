310dc35ed8fc5d39d1ac2f6fa83351a3d9057357
Solomon <solomonf1227@gmail.com>
2026-01-14 09:01:06 +0300

    feat: implement mutation for updating low stock products with logging

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,12 +1,51 @@
 import graphene
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+from graphene_django import DjangoObjectType
+from crm.models import Product
 
-class Query(CRMQuery, graphene.ObjectType):
+
+# --------------------------------------------------
+# Product Type
+# --------------------------------------------------
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = ("id", "name", "stock")
+
+
+# --------------------------------------------------
+# Mutation: UpdateLowStockProducts
+# --------------------------------------------------
+class UpdateLowStockProducts(graphene.Mutation):
+    success = graphene.String()
+    products = graphene.List(ProductType)
+
+    def mutate(self, info):
+        low_stock_products = Product.objects.filter(stock__lt=10)
+        updated_products = []
+
+        for product in low_stock_products:
+            product.stock += 10
+            product.save()
+            updated_products.append(product)
+
+        return UpdateLowStockProducts(
+            success="Low stock products updated successfully",
+            products=updated_products,
+        )
+
+
+# --------------------------------------------------
+# Root Query (can be empty if already defined elsewhere)
+# --------------------------------------------------
+class Query(graphene.ObjectType):
     pass
 
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
+
+# --------------------------------------------------
+# Root Mutation
+# --------------------------------------------------
+class Mutation(graphene.ObjectType):
+    update_low_stock_products = UpdateLowStockProducts.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
-
-
